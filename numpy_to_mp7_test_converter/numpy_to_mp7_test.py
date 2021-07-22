9237c35462923d49ae25eaf78c3b2b22f18471fa
Nicolo Ghielmetti <nghielme@yavin.cern.ch>
2021-07-22 15:51:05 +0200

    Added some input/output files targeting the VCU118 board. The parser has been extended to produce to_float(to_fixed(input)) values, useful to avoid casting issues with hls_model.predict(input). It has been also extended to generate 64 bits words per each link.

--- numpy_to_mp7_test_converter/numpy_to_mp7_test.py
+++ numpy_to_mp7_test_converter/numpy_to_mp7_test.py
@@ -1,26 +1,20 @@
 import argparse
 from ast import parse
 import numpy as np
-from rig.type_casts import NumpyFloatToFixConverter
+from rig.type_casts import NumpyFixToFloatConverter, NumpyFloatToFixConverter
 import bitstring
 
-'''
-def to_fixed(f,e):
-    a = f* (2**e)
-    b = int(round(a))
-    if a < 0:
-        # next three lines turns b into it's 2's complement.
-        b = abs(b)
-        b = ~b
-        b = b + 1
-    return b
-'''
 def c2(val):
-    return bitstring.pack('uint:16, int:16', 0, val).hex
+    if args.link_bitwidth == 32:
+        return bitstring.pack('uint:16, int:16', 0, val).hex
+    else:
+        return bitstring.pack('uint:16, uint:16, uint:16, int:16', 0, 0, 0, val).hex
 
 parser = argparse.ArgumentParser(description='Parse numpy file to FPGA testing for MP7 board')
 parser.add_argument('--board_name', type=str, help='A string representing the name of the board')
-parser.add_argument('--link_range', choices=range(0,73), type=int, nargs=2, metavar=('start','stop'), help='Start and stop values for the range related to links')
+parser.add_argument('--generate_float_from_fix', type=bool, help='Specify if you want to obtain the .npy file obtained via to_float(to_fix(input)). It is useful to feed to avoid casting mismatches using hls_model.predict()')
+parser.add_argument('--link_range', choices=range(0,96), type=int, nargs=2, metavar=('start','stop'), help='Start and stop values for the range related to links')
+parser.add_argument('--link_bitwidth', choices=[32,64], type=int, help='Word size in bits of each link')
 parser.add_argument('--invalid_rows', type=int, help='The number of invalid that will be generate at the beginning of the test')
 parser.add_argument('--fixed_point_bits', type=int, nargs=2, metavar=('word_bits', 'integer_bits'), help='The number of invalid that will be generate at the beginning of the test')
 parser.add_argument('--input_data_path', type=str, help='The path of the numpy file containing data in floating point')
@@ -35,6 +29,10 @@ if fp32_data.shape[0] > 1024:
 output_file = open(args.output_data_path, 'w')
 to_fixed = NumpyFloatToFixConverter(signed=True, n_bits=args.fixed_point_bits[0], n_frac=args.fixed_point_bits[0] - args.fixed_point_bits[1])
 fixed_data = to_fixed(fp32_data)
+if args.generate_float_from_fix:
+    to_float = NumpyFixToFloatConverter(n_frac=args.fixed_point_bits[0] - args.fixed_point_bits[1])
+    float_back_data = to_float(fixed_data)
+    np.save('float_from_fix.npy', float_back_data)
 c2_numpy = np.vectorize(c2)
 fixed_data = c2_numpy(fixed_data)
 
@@ -44,7 +42,10 @@ board_string = 'Board {}\n'.format(args.board_name)
 # channel section
 quad_chan_string = ' Quad/Chan : '
 for i in range(args.link_range[0], args.link_range[1] + 1):
-    quad_chan_string += '   q{:02d}c{}  '.format(i // 4, i % 4)
+    if args.link_bitwidth == 32:
+        quad_chan_string += '   q{:02d}c{}  '.format(i // 4, i % 4)
+    else:
+        quad_chan_string += '      q{:02d}c{}       '.format(i // 4, i % 4)
     if i != args.link_range[1]:
         quad_chan_string += ' '
     else:
@@ -53,7 +54,10 @@ for i in range(args.link_range[0], args.link_range[1] + 1):
 # link section 
 link_string = '      Link : '
 for i in range(args.link_range[0], args.link_range[1] + 1):
-    link_string += '    {:02d}    '.format(i)
+    if args.link_bitwidth == 32:
+        link_string += '    {:02d}    '.format(i)
+    else:
+        link_string += '        {:02d}        '.format(i)
     if i != args.link_range[1]:
         link_string += ' '
     else:
@@ -66,7 +70,10 @@ if args.invalid_rows > 0:
     for i in range(0,args.invalid_rows):
         frame += frame_start.format(i)
         for j in range(0, args.link_range[1] - args.link_range[0] + 1):
-            frame += '0v00000000'
+            if args.link_bitwidth == 32:
+                frame += '0v00000000'
+            else:
+                frame += '0v0000000000000000'
             if j != args.link_range[1] - args.link_range[0]:
                 frame += ' '
             else:
@@ -79,13 +86,14 @@ for i, v in enumerate(fixed_data):
         frame += ' '
     if dummy_cols > 0:
         for s in range(0, dummy_cols + 1):
-            frame += '1v00000000'
+            if args.link_bitwidth == 32:
+                frame += '0v00000000'
+            else:
+                frame += '0v0000000000000000'
             if s + j != args.link_range[1] - args.link_range[0] - 1:
                 frame += ' '
-    frame += '\n'        
-            
-
-        
+    frame += '\n'
+       
 l = [board_string, quad_chan_string, link_string, frame]
 output_file.writelines(l)
 output_file.close()
