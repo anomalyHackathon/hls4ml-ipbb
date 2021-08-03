295f1ce505c15846bebbe0fbf47ca2a9cc72fde8
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2021-08-03 16:11:32 +0200

    Create setup.py for compatibility purposes

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='hls4ml-ipbb',
+    version='0.0.1',
+    author='Maksymilian Graczyk (CERN)',
+    description='An automatic ipbb package for hls4ml IPs',
+    package_dir={'': '.'},
+    packages=['hls4ml_ipbb'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': ['ipbb_convert=ipbb_convert:main']
+    }
+)
