e0829f9a6b2ad429283f61837897815405726f72
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2021-08-03 17:41:57 +0200

    Make numpy_to_mp7.py comply with 80-column rule

--- numpy_to_mp7.py
+++ numpy_to_mp7.py
@@ -15,7 +15,9 @@ def to_float(x):
 def vals_to_hex(vals):
     nb = args.fixed_point_bits[0] # bits of one value
     tnb = len(vals) * nb # bitwidth of N values
-    assert args.link_bitwidth >= tnb, "Attempting to pack {} x {} bits ({} bits) into {}".format(len(vals), nb, tnb, args.link_bitwidth)
+    assert args.link_bitwidth >= tnb, \
+        "Attempting to pack {} x {} bits ({} bits) into {}".format(
+            len(vals), nb, tnb, args.link_bitwidth)
     pad = args.link_bitwidth - tnb
     fmt_string = 'uint:{},'.format(pad) + 'int:{},'.format(nb) * len(vals)
     return bitstring.pack(fmt_string, 0, *vals).hex
@@ -28,17 +30,36 @@ def row_to_hex(row):
     return y
 
 def main():
-    parser = argparse.ArgumentParser(description='Parse numpy file to FPGA testing for MP7 board')
-    parser.add_argument('--board_name', type=str, help='A string representing the name of the board')
-    parser.add_argument('--generate_float_from_fix', type=bool, help='Specify if you want to obtain the .npy file obtained via to_float(to_fix(input)). It is useful to feed to avoid casting mismatches using hls_model.predict()')
-    parser.add_argument('--link_range', choices=range(0,96), type=int, nargs=2, metavar=('start','stop'), help='Start and stop values for the range related to links')
-    parser.add_argument('--link_bitwidth', choices=[32,64], type=int, help='Word size in bits of each link')
-    parser.add_argument('--invalid_rows', type=int, help='The number of invalid that will be generate at the beginning of the test')
-    parser.add_argument('--fixed_point_bits', type=int, nargs=2, metavar=('word_bits', 'integer_bits'), help='The number of invalid that will be generate at the beginning of the test')
-    parser.add_argument('--pack_links', type=bool, help='Whether to pack multiple values into one link where possible')
+    parser = argparse.ArgumentParser(
+        description='Parse numpy file to FPGA testing for MP7 board')
+    parser.add_argument('--board_name', type=str,
+                        help='A string representing the name of the board')
+    parser.add_argument('--generate_float_from_fix', type=bool,
+                        help='Specify if you want to obtain the .npy file '
+                        'obtained via to_float(to_fix(input)). It is useful to '
+                        'feed to avoid casting mismatches using '
+                        'hls_model.predict()')
+    parser.add_argument('--link_range', choices=range(0,96), type=int, nargs=2,
+                        metavar=('start','stop'), help='Start and stop values '
+                        'for the range related to links')
+    parser.add_argument('--link_bitwidth', choices=[32,64], type=int,
+                        help='Word size in bits of each link')
+    parser.add_argument('--invalid_rows', type=int,
+                        help='The number of invalid that will be generate at '
+                        'the beginning of the test')
+    parser.add_argument('--fixed_point_bits', type=int, nargs=2,
+                        metavar=('word_bits', 'integer_bits'),
+                        help='The number of invalid that will be generate at '
+                        'the beginning of the test')
+    parser.add_argument('--pack_links', type=bool, help='Whether to pack '
+                        'multiple values into one link where possible')
     parser.add_argument('--link_map', type=int, nargs='*', help='The link map')
-    parser.add_argument('--input_data_path', type=str, help='The path of the numpy file containing data in floating point')
-    parser.add_argument('--output_data_path', type=str, help='The path of the produced .txt file containing data in fixed point')
+    parser.add_argument('--input_data_path', type=str,
+                        help='The path of the numpy file containing data in '
+                        'floating point')
+    parser.add_argument('--output_data_path', type=str,
+                        help='The path of the produced .txt file containing '
+                        'data in fixed point')
     args = parser.parse_args()
 
     fp32_data = np.load(args.input_data_path)
@@ -46,9 +67,12 @@ def main():
     pf = args.link_bitwidth // args.fixed_point_bits[0] if args.pack_links else 1
     link_width = args.link_range[1] - args.link_range[0] + 1
     if fp32_data.shape[1] > link_width * pf:
-        raise Exception('Trying to fit {} features into {} links with packing factor {}'.format(fp32_data.shape[1],link_width,pf))
+        raise Exception(
+            'Trying to fit {} features into {} links with packing factor {}'
+            .format(fp32_data.shape[1],link_width,pf))
     if fp32_data.shape[0] > 1024:
-        print('The system expect no more than 1024 rows; the original file will be truncated, keeping the first 1024 rows')
+        print('The system expect no more than 1024 rows; the original file will '
+              'be truncated, keeping the first 1024 rows')
         fp32_data = fp32_data[:1024]
 
     output_file = open(args.output_data_path, 'w')
@@ -59,8 +83,11 @@ def main():
         np.save('float_from_fix.npy', float_back_data)
     fixed_data = np.array([row_to_hex(row) for row in fixed_data])
 
-    link_map = list(range(args.link_range[0], args.link_range[1] + 1)) if args.link_map is None else args.link_map
-    assert len(link_map) == link_width, 'Link map length ({}) does not match link range ({})'.format(len(link_map), link_width)
+    link_map = list(range(args.link_range[0], args.link_range[1] + 1)) \
+        if args.link_map is None else args.link_map
+    assert len(link_map) == link_width, \
+        'Link map length ({}) does not match link range ({})'.format(
+            len(link_map), link_width)
 
     # board section
     board_string = 'Board {}\n'.format(args.board_name)
