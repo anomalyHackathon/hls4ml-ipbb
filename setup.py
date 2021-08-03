f9b3676b0a5619568413430ef8d8090c8525fbef
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2021-08-03 16:38:19 +0200

    Add hls4ml-ipbb dependencies to setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     package_dir={'': '.'},
     packages=['hls4ml_ipbb'],
     python_requires='>=3.6',
+    install_requires=['defusedxml==0.7.1'],
     entry_points={
         'console_scripts': ['ipbb_convert=ipbb_convert:main']
     }
